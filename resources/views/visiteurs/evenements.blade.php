9c50a9b64819f46fd6cae0d8c24f7df8835a6892
Cyril29D <cyrildelly9@gmail.com>
2024-05-06 14:14:38 +0000

    devoir E5

--- resources/views/visiteurs/evenements.blade.php
+++ resources/views/visiteurs/evenements.blade.php
@@ -9,43 +9,30 @@
         <header class="section background-dark">
           <div class="line">        
             <h1 class="text-white margin-top-bottom-40 text-size-60 text-line-height-1">Evènements à la maison des ligues.</h1>
-       
+            <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
+<script src="{{ asset('/js/bootstrap.bundle.min.js') }}"></script>
+
           </div>  
         </header>
         <div class="section background-white"> 
-          <div class="line">  
-            <h2 class="text-size-40 margin-bottom-30">Premier</h2>
-            <hr class="break-small background-primary margin-bottom-30">
-          
-            <div class="line margin-top-30">
-              <div class="margin">
-                <div class="s-12 m-6 l-6">
-                  <img src="{{ asset('/img/evenements/eve1.jpg') }}" alt="">
-                
-                </div>
-                <div class="s-12 m-6 l-6">
-                  <p class="margin-top-30">
-                  Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; 
-                  est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. 
-                  Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc 
-                  putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc 
-                  nobis videntur parum clari, fiant sollemnes in futurum.
-                  </p>
-                </div>
-              </div>
-            </div>      
-          </div>
-        </div>
         @foreach  ($evenements as $events)
+
+        
+  
         <div class="section background-white"> 
           <div class="line">  
-            <h3 class="text-size-40 margin-bottom-30">{{ $events->intitule }}/ {{ $events->lieu }}</h2>
+          <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          {{ $events->intitule }}/ {{ $events->lieu }}
+    </button>
+    <ul class="dropdown-menu">
+    <li><h3 class="text-size-40 margin-bottom-30"></h2></li>
             <h4>{{ $events->date_deb }} / {{ $events->date_fin }}</h3>
             <hr class="break-small background-primary margin-bottom-30">
-          
+            
             <div class="line margin-top-30">
               <div class="margin">
-                <div class="s-12 m-6 l-6">
+                <div class="s-9 m-6 l-6">
               <img src="{{ asset('/img/evenements/'.$events->url_image) }}" alt="">
 
               </div>
@@ -53,7 +40,7 @@
                   <p class="margin-top-30">
                   {{ $events->details }}
                   </p>
-                  
+                  </option>
                 </div>
               </div>
             </div>      
